3a070a68a3ef93fa72be370c7bd5444afb936596
Seppe Dekeyser <seppe@cloudclass.be>
2023-05-08 09:46:29 +0200

    Add reproduction

---
+++ e2e/kit/src/routes/default-fetching-bug/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { DefaultFetchingBugStore } from '$houdini';
+
+  $: store = new DefaultFetchingBugStore();
+</script>
+
+<button on:click={() => store.fetch()}>Click to load</button>
+
+<pre>{JSON.stringify({ fetching: $store.fetching, data: $store.data })}</pre>
